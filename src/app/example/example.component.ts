b1a9239fa621dc1a16d75cbdda09dd93eea69608
Dhanar J Kusuma <Dhanar J Kusuma>
2017-05-13 12:15:49 +0700

    initial commit

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-example',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExampleComponent implements OnInit {
 
+  @Input() nilai : string
+
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }

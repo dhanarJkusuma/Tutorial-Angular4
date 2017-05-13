25faa1b9d932e99f9f979ac4c6ed76004ebeafe4
Dhanar J Kusuma <Dhanar J Kusuma>
2017-05-14 02:17:19 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Mahasiswa } from './mahasiswa.interface';
+
 
 @Component({
   selector: 'app-root',
@@ -9,24 +8,4 @@ import { Mahasiswa } from './mahasiswa.interface';
 })
 export class AppComponent{
   title = 'app works!';
-  mahasiswa : FormGroup;
-
-  constructor(private mhs : FormBuilder){
-    this.createForm();
-  }
-
-  createForm(){
-    this.mahasiswa = this.mhs.group({
-      nama_mhs: '',
-      pendidikan : this.mhs.group({
-        nama_fakultas: '',
-        nama_prodi: ''
-      })
-    })
-  }
-
-   onSubmit(f : FormGroup) {
-    console.log("Hasil dari value dari form adalah");
-    console.log(f.value);
-  }
 }

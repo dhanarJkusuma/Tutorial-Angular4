b62ea5cd096edd6c3389315790f82416c5806f1b
Dhanar J Kusuma <Dhanar J Kusuma>
2017-05-14 02:01:29 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule  } from '@angular/forms';
+import { FormsModule  } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule ,
+    FormsModule ,
     HttpModule
   ],
   providers: [],
--- src/app/mahasiswa.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Mahasiswa{ //FormGroup
-    nama_mhs: string; //FormControl
-    pendidikan: { //FormGroup
-        nama_fakultas: string; //FormControl
-        nama_prodi: string; //FormControl
-    }
-}
